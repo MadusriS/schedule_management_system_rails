9bb4b23956c4c25a22cfc9ee7dd8d13f4824f457
MadusriS <madusri@mobicip.com>
2024-04-22 19:18:05 +0530

    authentication done

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -1,8 +1,4 @@
 module SchedulesHelper
-    def schedule_days(days)
-      day_names = { 1 => 'Sunday', 2 => 'Monday', 4 => 'Tuesday', 8 => 'Wednesday', 16 => 'Thursday', 32 => 'Friday', 64 => 'Saturday' }
-      selected_days = day_names.select { |key, _value| (days & key) != 0 }.values
-      selected_days.join(', ')
-    end
+
   end
   
